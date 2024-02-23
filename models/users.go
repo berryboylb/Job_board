592d35b0746accb3589c22cbfb4432bcd482781f
berryboylb <phemmynesce4life@gmail.com>
2024-02-24 00:10:42 +0100

    @chore: profile

--- models/users.go
+++ models/users.go
@@ -2,20 +2,27 @@ package models
 
 import (
 	"github.com/google/uuid"
+	"github.com/lib/pq"
 	"gorm.io/gorm"
 	// "log"
 	// "os"
-	// "time"
 )
 
-//user struct
+type RoleAllowed string
+
+const (
+	AdminRole  RoleAllowed = "admin"
+	PosterRole RoleAllowed = "poster"
+	UserRole   RoleAllowed = "user"
+)
+
+// user struct
 type User struct {
 	gorm.Model
-	ID                uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	ID         uuid.UUID      `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	Name       string         `gorm:"name"`
+	Email      string         `gorm:"uniqueIndex"`
+	Picture    string         `json:"picture"`
+	RoleName   RoleAllowed    `json:"role" sql:"type:role_name"`
+	RoleAccess pq.StringArray `json:"role_access" gorm:"type:text[]; not null"`
 }
-
-
-
-
-
-
