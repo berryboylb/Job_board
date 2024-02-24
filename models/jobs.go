4b75f465f1b34e8c1ac647d7ad21171510da144d
berryboylb <phemmynesce4life@gmail.com>
2024-02-24 02:39:42 +0100

    @chore:jobs

--- models/jobs.go
+++ models/jobs.go
@@ -8,8 +8,19 @@ import (
 	// "time"
 )
 
-//user struct
+type JobType struct {
+	gorm.Model
+	ID   uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	Name string    `gorm:"type:varchar(250);not null; uniqueIndex"`
+}
+
 type Jobs struct {
 	gorm.Model
-	ID                uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	ID             uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	Title          string    `gorm:"type:varchar(100);not null"`
+	Description    string    `gorm:"type:LONGTEXT;not null"`
+	Location       string    `gorm:"type:varchar(250);not null"`
+	Salary         float64   `gorm:"type:decimal(10,2);default:0.0"`
+	JobTypeID      uuid.UUID `gorm:"type:uuid;not null"`
+	JobType        JobType   `gorm:"foreignKey:JobTypeID"`
 }
