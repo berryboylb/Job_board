31f060761ae16d3a21af90b04c459609d150ce6c
berryboylb <phemmynesce4life@gmail.com>
2024-03-01 21:25:51 +0100

    @chore: user routes

--- user/routes.go
+++ user/routes.go
@@ -2,17 +2,21 @@ package user
 
 import (
 	"github.com/gin-gonic/gin"
-)
 
+	"job_board/jwt"
+)
 
 // New registers the routes and returns the router.
 func UserRoutes(superRoute *gin.RouterGroup) {
 
 	userRouter := superRoute.Group("/users")
 	{
-		userRouter.POST("/user", User)
+		userRouter.Use(jwt.Middleware())
+		userRouter.GET("/", GetAllUsers)
+		userRouter.GET("/user", User)
+		userRouter.POST("/", CreateAdmin)
+		userRouter.PATCH("/", UpdateUser)
+		userRouter.DELETE("/", DeleteUser)
 	}
 
 }
-
-
