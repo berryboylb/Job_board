3b3c16284157ec4c1461d111da3655fba8477324
berryboylb <phemmynesce4life@gmail.com>
2024-03-01 22:01:40 +0100

    @chore: response

--- user/controller.go
+++ user/controller.go
@@ -1,21 +1,39 @@
 package user
 
 import (
-	"net/http"
 	"github.com/gin-gonic/gin"
+	"net/http"
+
+	"job_board/models"
 )
 
 func User(ctx *gin.Context) {
 	//i just want to get the user data
 
+	value, exists := ctx.Get("user")
+	if !exists {
+		ctx.String(http.StatusUnauthorized, "User not found in session")
+		return
+	}
+
+	user, ok := value.(models.User)
+	if !ok {
+		ctx.String(http.StatusInternalServerError, "Mismatching types")
+		ctx.Abort()
+		return
+	}
+	// Now 'user' contains the user if it exists, and you can proceed with further processing
+
 	ctx.JSON(http.StatusOK, gin.H{
-		"data": "user data",
+		"message":    "successfully fetched user",
+		"data":       user,
+		"statusCode": http.StatusOK,
 	})
 }
 
 func GetAllUsers(ctx *gin.Context) {
 	//i just want to get the user data
-	
+
 	ctx.JSON(http.StatusOK, gin.H{
 		"data": "user data",
 	})
@@ -23,7 +41,7 @@ func GetAllUsers(ctx *gin.Context) {
 
 func CreateAdmin(ctx *gin.Context) {
 	//i just want to get the user data
-	
+
 	ctx.JSON(http.StatusOK, gin.H{
 		"data": "user data",
 	})
@@ -31,19 +49,16 @@ func CreateAdmin(ctx *gin.Context) {
 
 func UpdateUser(ctx *gin.Context) {
 	//i just want to get the user data
-	
+
 	ctx.JSON(http.StatusOK, gin.H{
 		"data": "user data",
 	})
 }
-
 
 func DeleteUser(ctx *gin.Context) {
 	//i just want to get the user data
-	
+
 	ctx.JSON(http.StatusOK, gin.H{
 		"data": "user data",
 	})
 }
-
-
